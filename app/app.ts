131bc12ab9a1d3ba7f0157d2dab532a1fc5b2427
unknown <shcherbakovkirill2@gmail.com>
2024-05-11 15:12:40 +0300

    fead: add linting

--- app/app.ts
+++ app/app.ts
@@ -1,22 +1,22 @@
-import * as express from "express";
-import * as cors from "cors";
-import * as cookieParser from "cookie-parser";
-import { config } from "dotenv";
+import * as express from 'express'
+import * as cors from 'cors'
+import * as cookieParser from 'cookie-parser'
+import { config } from 'dotenv'
 
 async function bootstrap() {
   try {
-    config();
+    config()
 
-    const app = express();
+    const app = express()
 
-    app.use(cors());
-    app.use(express.json());
-    app.use(cookieParser());
+    app.use(cors())
+    app.use(express.json())
+    app.use(cookieParser())
 
-    app.listen(5050, () => console.log(`[server]: Listen to 5050 port`));
+    app.listen(5050, () => console.log(`[server]: Listen to 5050 port`))
   } catch (e) {
-    console.log(`Initial server error: ${e}`);
+    console.log(`Initial server error: ${e}`)
   }
 }
 
-bootstrap();
+bootstrap()
