b3f6799e4a6b54fffba21cac26561dd639fb7059
unknown <shcherbakovkirill2@gmail.com>
2024-05-11 15:07:17 +0300

    fead: add app.ts

---
+++ app/app.ts
@@ -0,0 +1,22 @@
+import * as express from "express";
+import * as cors from "cors";
+import * as cookieParser from "cookie-parser";
+import { config } from "dotenv";
+
+async function bootstrap() {
+  try {
+    config();
+
+    const app = express();
+
+    app.use(cors());
+    app.use(express.json());
+    app.use(cookieParser());
+
+    app.listen(5050, () => console.log(`[server]: Listen to 5050 port`));
+  } catch (e) {
+    console.log(`Initial server error: ${e}`);
+  }
+}
+
+bootstrap();
